231e389fd1dd3ffa4f1c55974aee3ecb690f9c0e
Seongeun Hong <37788658+Seoneun@users.noreply.github.com>
2021-11-12 20:54:01 +0900

    Fix typo

--- nlp/nlp.py
+++ nlp/nlp.py
@@ -1,22 +1,9 @@
-import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import re
 import fasttext
-import nltk
 import pandas as pd
 import kss
 import json
-
-model = fasttext.load_model('drive/MyDrive/Colab Notebooks/cc.ko.300.bin')
-nltk.download('treebank')
-nltk.download('punkt')
-
-def plot_2d_graph(vocabs, xs, ys):
-  plt.rc('font', family='NanumBarunGothic') 
-  plt.figure(figsize=(8,6))
-  plt.scatter(xs, ys, marker = 'o')
-  for i, v in enumerate(vocabs):
-    plt.annotate(v, xy = (xs[i], ys[i]))
 
 def remove_tag(content):
    
@@ -25,26 +12,37 @@ def remove_tag(content):
 
    return cleantext
 
-def nlp(data, model):
+model_dir = ''
+
+model = fasttext.load_model(model_dir)
+
+def nlp(data, model = fasttext.load_model(model_dir)):
+
+    # .json파일일 경우 주석 제거
     '''
-    file_name = 'twitter_data.json'
+    file_name = 'twitterdata-1.json'
     with open(file_name, "r") as st_json:
       data = json.load(st_json)
     '''
+
     data_content = []
-    pre_sentence = {'코로나': ['코로나 신규 확진자가 발생했습니다'], '화재 발생': ['불 났대'], '홍수 발생': ['물이 많아'], '교통사고 발생': ['사고 났다.']}
+    data_date = []
+    data_link = []
+    data_type = []
+
+    pre_sentence = {'코로나': ['코로나 신규 확진자가 발생했습니다'], '화재': ['불 났대'], '홍수': ['물이 많아'], '교통사고': ['사고 났다'], '폭설': ['눈이 많이 왔습니다'], '산불': ['산에 불이 발생했습니다'], '붕괴': ['붕괴 사고가 발생했습니다'], '폭발': ['폭발 했습니다'], '태풍': ['태풍이 옵니다'] }
 
     for record in data:
       data_content.append(record['content'])
+      data_date.append(record['date'])
+      data_link.append(record['link'])
+      data_type.append(record['type'])
 
     for index in range(len(data_content)):
-      try:
-        if len(data_content[index]) > 0:
+      if len(data_content[index]) > 0:
           data_content[index] = pre_sentence[list(pre_sentence.keys())[list(pre_sentence.keys()).index(data_type[index])]] + kss.split_sentences(remove_tag(data_content[index]))
-        else:
+      else:
           data_content[index] = []
-      except:
-        data_content[index] = []
 
     content_vector = []
     index = -1
@@ -54,8 +52,6 @@ def nlp(data, model):
       content_vector.append([])
       for sentence in content:
         content_vector[index].append(model.get_sentence_vector(sentence.replace('\n', '')))
-
-    del_count = 0
 
     for vectors in range(len(content_vector)):
       try:
@@ -75,12 +71,11 @@ def nlp(data, model):
             break
           if kmeans.labels_[index] != target_label:
             del data_content[vectors][index]
-            del_count = del_count + 1
         del  data_content[vectors][0]
       except:
         data_content[vectors] = []
-      
-    for index in range(len(data)):
-      data[index]['content'] = data_content[index]
 
-    return data
+    for index in range(len(data)):
+      data[index]['content'] = data_content[index] 
+
+   return data
