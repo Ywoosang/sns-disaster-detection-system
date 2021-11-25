795c9dea76824079f45e6623275107180506406c
Ywoosang <opellong13@gmail.com>
2021-11-25 14:58:22 +0900

    Refactor architecture

--- naver-service/src/toISO.js
+++
@@ -1,6 +0,0 @@
-exports.toISO = function (arr){
-    var ti = new Date(arr[0],(Number(arr[1])-1).toString(),arr[2],hours = arr[3],minutes = arr[4])
-    var res = new Date(ti.getTime() + 9 * 60 * 60 * 1000)
-    console.log(res)
-    return res.toISOString()
-}
