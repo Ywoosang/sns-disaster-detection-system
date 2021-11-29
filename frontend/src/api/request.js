1f2ceb290f2c4a9b3b38efb67124d684ba9035f0
Ywoosang <opellong13@gmail.com>
2021-11-29 19:30:43 +0900

    Add mail request

--- frontend/src/api/request.js
+++ frontend/src/api/request.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const postMail = async(manager,mailText) => {
     axios({
         method: "post",
-        url: "https://disasterback.cf/api/mail",
+        url: "http://localhost:8080/api/mail",
         data: {
           mailText: mailText,
           manager: manager,
